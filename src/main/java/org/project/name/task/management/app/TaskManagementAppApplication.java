6d4e952cdbdf52f0d7dcb2a1266739a8ceedf950
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2023-11-07 10:34:41 +0200

    created a spring boot project

---
+++ src/main/java/org/project/name/task/management/app/TaskManagementAppApplication.java
@@ -0,0 +1,13 @@
+package org.project.name.task.management.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementAppApplication.class, args);
+    }
+
+}
