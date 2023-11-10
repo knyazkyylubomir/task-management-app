4e62f3f1994b00caa5097ce70cbb8651b8de92a6
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2023-11-10 23:49:30 +0200

    added fixes

--- src/main/java/org/project/name/task/management/app/repository/user/UserRepository.java
+++ src/main/java/org/project/name/task/management/app/repository/user/UserRepository.java
@@ -3,7 +3,9 @@ package org.project.name.task.management.app.repository.user;
 import java.util.Optional;
 import org.project.name.task.management.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
 
--- src/test/java/org/project/name/task/management/app/TaskManagementAppApplicationTests.java
+++ src/test/java/org/project/name/task/management/app/TaskManagementAppApplicationTests.java
@@ -3,7 +3,7 @@ package org.project.name.task.management.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TaskManagementAppApplicationTests.class)
 class TaskManagementAppApplicationTests {
 
     @Test
