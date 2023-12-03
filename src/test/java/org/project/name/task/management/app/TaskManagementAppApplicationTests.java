434fe204b3de63180446e3e53555cf7c92f5ee5b
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2023-12-03 14:59:38 +0200

    added some fix

--- src/test/java/org/project/name/task/management/app/TaskManagementAppApplicationTests.java
+++ src/test/java/org/project/name/task/management/app/TaskManagementAppApplicationTests.java
@@ -3,7 +3,7 @@ package org.project.name.task.management.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = TaskManagementAppApplicationTests.class)
+@SpringBootTest
 class TaskManagementAppApplicationTests {
 
     @Test
